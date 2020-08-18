47813c4c544edce4155302925f4d0e404a3ec0b8
Zidaan16 <43307788+Zidaan16@users.noreply.github.com>
2020-08-18 23:33:02 +0700

    Add files via upload

---
+++ bintang2.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    for(int a = 5; a >= 1; a--){
+        for(int b = 1; b <= a; b++){
+            cout << "*";
+        }
+        cout << endl;
+    }
+}
+
+/*
+Output:
+
+*****
+****
+***
+**
+*
+
+*/
